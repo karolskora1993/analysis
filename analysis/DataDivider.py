3e0cfe8dd2137cd1e3b82852c9cdfcb2fb3880b1
karolskora1993 <karol.skora@wingu.de>
2017-06-23 21:45:19 +0200

    +divide data into blocks

--- analysis/DataDivider.py
+++ analysis/DataDivider.py
@@ -1,15 +1,56 @@
 import pandas as pd
-import numpy as np
-from analysis import blocks
 
+BLOCKS_PATH = '/Users/apple/Desktop/mag/bloki_poprawione.xlsx'
+DATA_PATH = '/Users/apple/Desktop/mag/dane/DANE_PO_MODERNIZACJI_VRM/wypelnione/csv_all_v2.csv'
+VARS = '/Users/apple/Desktop/mag/dane/DANE_PO_MODERNIZACJI_VRM/kolumny.xlsx'
+DEST_PATH = '/Users/apple/Desktop/mag/dane/DANE_PO_MODERNIZACJI_VRM/wypelnione/bloki/'
+
+class DataDivider:
+
+    blocks_data = None
+    block_names = None
+    data = None
+    dest_path = None
+
+    def __init__(self, blocks_path, block_names, data_path, destination_path):
+        self.blocks_data = pd.read_excel(blocks_path, sheetname=block_names)
+        self.blocks_names = block_names
+        self.data = pd.read_csv(data_path)
+        self.dest_path = destination_path
+        print('data loaded')
+
+    def find_mismatch(self):
+        variables = self.data.columns.values
+        mismatches = []
+        for key, value in self.blocks_data.items():
+            for name in value['zmienne']:
+                if '#' in name:
+                    continue
+                match = None
+                for var in variables:
+                    if name == var:
+                        match = var
+                if match is None:
+                    print('Nie znaleziono pasujacego dla zmiennej {0}'.format(name))
+                    mismatches.append(name)
+        if len(mismatches)>0:
+            print("Nie dopasowane:{0}".format(mismatches))
+        return mismatches if len(mismatches) > 0 else None
+
+    def divide_into_groups(self):
+        if not self.find_mismatch():
+            for key in self.blocks_data.keys():
+                block_vars = []
+                for var_name in self.blocks_data[key]['zmienne']:
+                    if not '#' in var_name:
+                        block_vars.append(var_name)
+                block_df = self.data[block_vars]
+                block_df.to_csv(self.dest_path+key+'.csv')
+                print('{0} zapisany'.format(key))
 
 def main():
-    df = pd.read_excel(blocks.PATH, 'Arkusz1')
-    print("dataframe created")
-    series = [df[s] for s in blocks.names[0]]
-    print(series)
-    resultDf = pd.DataFrame(np.asarray(series), columns=names[0])
-    print(resultDf)
+    data_divider = DataDivider(BLOCKS_PATH, ['Blok I', 'Blok II', 'Blok III', 'Blok IV'], DATA_PATH, DEST_PATH)
+    data_divider.divide_into_groups()
 
 if __name__ == '__main__':
     main()
