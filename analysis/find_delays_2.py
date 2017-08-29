411504c4a9654350cf04666f93398856aad0633a
karolskora1993 <karol-skora@poczta.pl>
2017-08-29 19:11:56 +0200

    delays

---
+++ analysis/find_delays_2.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import os
+from sklearn import tree
+from numpy import nan
+import collections
+
+HOME_PATH = str(os.path.expanduser('~')+'/')
+
+VARS_PATH = HOME_PATH + '/Dokumenty/analysis/data/bloki_poprawione_v2.xlsx'
+LOAD_PATH = HOME_PATH + '/Dokumenty/analysis/data/bloki_v4/'
+SCORES_SAVE_PATH = HOME_PATH + '/Dokumenty/analysis/data/scores_v2.txt'
+BLOCK_VARS_PATH_XLSX = HOME_PATH + '/Dokumenty/analysis/data/bloki_poprawione.xlsx'
+
+names = [
+    'blok I',
+    'blok II',
+    'blok III',
+    'blok IV'
+]
+COL_NAMES = ['in', 'control', 'out']
+MAX_DELAY = 40
+
+
+def find_best(models):
+    best_score = None
+    best_model = None
+    for model in models:
+        if best_score is None or best_score < model['score']:
+            best_score = model['score']
+            best_model = model
+    return best_model, best_score
+
+
+def load_block_vars():
+    df = pd.read_excel(BLOCK_VARS_PATH_XLSX, sheetname=None)
+    print('blocks vars loaded')
+    return df
+
+
+def load_data(block_name):
+    df = pd.read_csv(LOAD_PATH + block_name + '.csv', index_col=0)
+    print('data loaded')
+    return df
+
+
+def most_common(delays):
+    return collections.Counter(delays).most_common()[0][0]
+
+
+def main():
+    blocks_vars = load_block_vars()
+    with open('delays.txt', 'w') as f:
+        for block_name in names:
+            print(block_name)
+            data = load_data(block_name)
+            vars = blocks_vars[block_name][COL_NAMES]
+            vars_in = [x for x in vars['in'] if x is not nan]
+            vars_control = [x for x in vars['control'] if x is not nan]
+            vars_in += vars_control
+            vars_out = [x for x in vars['out'] if x is not nan]
+            for var_out in vars_out:
+                y = ((data[var_out])[MAX_DELAY:]).as_matrix()
+                delays = []
+                for var_in in vars_in:
+                    if var_in is not None and var_in is not nan:
+                        models = []
+                        x = (data[var_in]).as_matrix()
+                        for delay in range(0, MAX_DELAY + 1):
+                            x_train = (x[MAX_DELAY - delay:-delay]).reshape(-1, 1)
+                            model = tree.DecisionTreeRegressor()
+                            model.fit(x_train, y)
+                            models.append({'delay': delay, 'score': model.score(x_train, y)})
+                        best_model = find_best(models)
+                        line = 'var_out: {0}  var_in: {1}  best delay: {2}  score:{3}'.format(var_out, var_in,
+                                                                                             best_model['delay'],
+                                                                                             best_model['score'])
+                        print(line)
+                        f.write(line)
+                        delays.append(best_model['delay'])
+                        line = 'var_out: {0} delay:{1}'.format(var_out, most_common(delays))
+                print(line)
+                f.write(line)
+
+
+if __name__ == '__main__':
+    main()
