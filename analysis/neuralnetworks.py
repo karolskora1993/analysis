606df152d615f579974f2b95d13ccf32f16eea17
karolskora1993 <karol-skora@poczta.pl>
2017-09-07 10:36:44 +0200

    neural networks- code refactoring

--- analysis/neuralnetworks.py
+++ analysis/neuralnetworks.py
@@ -1,28 +1,93 @@
 import pandas as pd
 from analysis import blocks
-from numpy import nan
 import numpy as np
 import pickle
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
 import sklearn.metrics as metrics
-from math import floor
-
-import lasagne
-from lasagne import layers
-from lasagne.updates import nesterov_momentum
-from nolearn.lasagne import NeuralNet
-
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout, Conv1D
-
+from keras.layers import Dense, Dropout
+from abc import ABC, abstractmethod
 import sys
 sys.setrecursionlimit(10000)
 
 COL_NAMES = ['in', 'control', 'out']
+LAST_TRAIN_IDX = 200.000
+LAST_VALIDATE_IDX = 250.000
 
-def simple_model(x, y, x_test, y_test, y_labels, score_file):
+
+class ModelTester(ABC):
+    @staticmethod
+    @abstractmethod
+    def test_model(model, x_test, y_test):
+        pass
+
+
+class Model(ABC):
+    def __init__(self, input_data, output_data, model_tester, network_shape, optimizer='adam', loss='mean_squared_error'):
+        self._input_data = input_data
+        self._output_data = output_data
+        self._model_tester = model_tester
+        self._network_shape = network_shape
+        self._optimizer = optimizer
+        self._loss = loss
+        self._model = None
+        self.validate_data()
+
+    def validate_data(self):
+        if np.any(np.isnan(self.input_data)) or np.any(np.isnan(self._output_data)):
+            raise ValueError('Data contains NaN values')
+        if not np.all(np.isfinite(self.input_data)) or not np.all(np.isfinite(self._output_data)):
+            raise ValueError('Data contains infinite values')
+
+    @abstractmethod
+    def create_model(self, input_size, output_size):
+        pass
+
+    @abstractmethod
+    def train_model(self, x_train, y_train, validation_data=None):
+        pass
+
+    @abstractmethod
+    def test_model(self, x_test, y_test):
+        pass
+
+
+class KerasMLPModel(Model):
+    def create_model(self, input_size, output_size):
+        self._model = Sequential()
+        self._model.add(Dense(50, input_dim=input_size))
+        self._model.add(Dropout(0.5))
+        self._model.add(Dense(50, activation='relu'))
+        self._model.add(Dense(output_size, activation='linear'))
+        self._model.compile(optimizer=self._optimizer, loss=self._loss)
+
+    def train_model(self, x_train, y_train, validation_data):
+        scaler = StandardScaler().fit(x_train)
+        x_train = scaler.transform(x_train)
+        self._model.fit(x_train, y_train, epochs=400, batch_size=32, verbose=2, validation_data=validation_data)
+        print('evaluate')
+
+
+def load_data(block_name):
+    df = pd.read_csv(blocks.LOAD_PATH + block_name + '.csv', index_col=0)
+    print('data loaded')
+    return df
+
+def get_score(predictions, y_test, y_labels, score_file):
+    columns = predictions.shape[1]
+    with open(score_file, 'w') as f:
+        for i in range(0, columns):
+            r2 = metrics.r2_score(y_test[:, i], predictions[:, i])
+            mse = metrics.mean_squared_error(y_test[:, i], predictions[:, i])
+            mae = metrics.mean_absolute_error(y_test[:, i], predictions[:, i])
+            ews = metrics.explained_variance_score(y_test[:, i], predictions[:, i])
+            print_line = 'zmienna {0}- r^2: {1}  mse: {2} mae: {3} ews: {4} \n'.format(y_labels[i], r2, mse, mae, ews)
+            print(print_line)
+            f.write(print_line)
+
+
+def sklearn_model(x, y, x_test, y_test, y_labels, score_file):
     mlp = MLPRegressor(hidden_layer_sizes=(100, 100, 100, 100, 100, 100), max_iter=100000)
     print('Any NaN: {0}'.format(np.any(np.isnan(x))))
     print('All finite: {0}'.format(np.all(np.isfinite(x))))
@@ -38,114 +103,10 @@ def simple_model(x, y, x_test, y_test, y_labels, score_file):
     print('fit')
     predictions = mlp.predict(x_test)
     print('predict')
-    score(predictions, y_test, y_labels, score_file)
+    get_score(predictions, y_test, y_labels, score_file)
     return mlp
 
 
-def score(predictions, y_test, y_labels, score_file):
-    columns = predictions.shape[1]
-    with open(score_file, 'w') as f:
-        for i in range(0, columns):
-            r2 = metrics.r2_score(y_test[:, i], predictions[:, i])
-            mse = metrics.mean_squared_error(y_test[:, i], predictions[:, i])
-            mae = metrics.mean_absolute_error(y_test[:, i], predictions[:, i])
-            ews = metrics.explained_variance_score(y_test[:, i], predictions[:, i])
-            print_line = 'zmienna {0}- r^2: {1}  mse: {2} mae: {3} ews: {4} \n'.format(y_labels[i], r2, mse, mae, ews)
-            print(print_line)
-            f.write(print_line)
-
-
-def load_data(block_name):
-    df = pd.read_csv(blocks.LOAD_PATH + block_name + '.csv', index_col=0)
-    print('data loaded')
-    return df
-
-
-def lasagne_model(x, y, x_test, y_test, y_labels, score_file):
-    x = np.transpose(np.matrix(x))
-    y = np.transpose(np.matrix(y))
-    net = NeuralNet(
-        layers=[('input', layers.InputLayer),
-                ('conv2d1', layers.Conv2DLayer),
-                ('dense', layers.DenseLayer),
-                ('dropout1', layers.DropoutLayer),
-                ('dense', layers.DenseLayer),
-                ('dropout2', layers.DropoutLayer),
-                ('output', layers.DenseLayer),
-                ],
-        # input layer
-        input_shape=(None, None, x.shape[0], x.shape[1]),
-        # layer conv2d1
-        conv2d1_incoming=(None, None, x.shape[0], x.shape[1]),
-        conv2d1_num_filters=32,
-        conv2d1_filter_size=(5, 5),
-        conv2d1_nonlinearity=lasagne.nonlinearities.linear,
-        conv2d1_W=lasagne.init.GlorotUniform(),
-        # dropout1
-        dropout1_p=0.5,
-        # dense
-        dense_num_units=256,
-        dense_nonlinearity=lasagne.nonlinearities.linear,
-        # dropout2
-        dropout2_p=0.5,
-        # output
-        output_nonlinearity=lasagne.nonlinearities.linear,
-        output_num_units=len(y_labels),
-        # optimization method params
-        update=nesterov_momentum,
-        update_learning_rate=0.01,
-        update_momentum=0.9,
-        max_epochs=200,
-        verbose=1,
-    )
-    print('create net')
-    nn = net.fit(x, y)
-    print('fit net')
-    predictions = net.predict(x_test)
-    score(y_test, predictions, y_labels, score_file)
-
-    return nn
-
-
-def keras_model(x, y, x_test, y_test, y_labels, score_file):
-    print('Keras model')
-    print('Any NaN: {0}'.format(np.any(np.isnan(x))))
-    print('All finite: {0}'.format(np.all(np.isfinite(x))))
-    x = np.transpose(np.matrix(x))
-    y = np.transpose(np.matrix(y))
-    x_test = np.transpose(np.matrix(x_test))
-    y_test = np.transpose(np.matrix(y_test))
-
-    scaler = StandardScaler().fit(x)
-    x = scaler.transform(x)
-    x_test = scaler.transform(x_test)
-    print('scale')
-    print('X:{0} Y:{1}'.format(x.shape, y.shape))
-
-    model = Sequential()
-    model.add(Dense(50, input_dim=x.shape[1]))
-    model.add(Dropout(0.5))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(len(y_labels), activation='linear'))
-
-    model.compile(optimizer='adam',
-                  loss='mean_squared_error')
-
-    model.fit(x, y, epochs=400, batch_size=32, verbose=2)
-    model.evaluate(x_test, y_test, batch_size=32)
-    print('evaluate')
-
-    predictions = model.predict(x_test)
-    score(predictions, y_test, y_labels, score_file)
-
-    return model
 
 
 def load_block_vars():
@@ -157,48 +118,32 @@ def load_block_vars():
 def save_model(model, file_name):
     pickle.dump(model, open(file_name, 'wb'))
 
+def model_single_output(input_data, output_data):
+    model = keras_model(x_train, y_train)
+
+
+
+def model_block(data, var_names):
+    vars_in = var_names['in'].append(var_names['control']).dropna().tolist()
+    vars_out = var_names['out'].dropna().tolist()
+    input_data = data[vars_in]
+    for var_out in vars_out:
+        output_data = data[var_out]
+        model = model_single_output(input_data, output_data)
+
 
 def main():
-    blocks_vars = load_block_vars()
+    block_vars = load_block_vars()
     for block_name in blocks.names:
         print(block_name)
-        df = load_data(block_name)
+        data = load_data(block_name)
+        var_names = block_vars[block_name][COL_NAMES]
 
-        vars = blocks_vars[block_name][COL_NAMES]
-        vars_in = [x for x in vars['in'] if not x is nan and not '#' in x]
-        vars_control = [x for x in vars['control'] if not x is nan and not '#' in x]
-        vars_all_in = vars_in + vars_control
-        vars_out = [x for x in vars['out'] if not x is nan and not '#' in x]
-        input_data = [np.array(df[x].tolist()) for x in vars_all_in]
-        output_data = [np.array(df[x].tolist()) for x in vars_out]
-        last_train_idx = floor(len(input_data[0]) * 0.875)
-        last_test_idx = len(input_data[0]) - 1
-        x_train = [x[:last_train_idx] for x in input_data]
-        y_train = [x[:last_train_idx] for x in output_data]
-        x_test = [x[last_train_idx:last_test_idx] for x in input_data]
-        y_test = [x[last_train_idx:last_test_idx] for x in output_data]
-
-        #sklearn model
-        # name = 'sklearn_model_default_6x100_{0}'.format(block_name)
-        # print('sklearn model {0}'.format(name))
-        # score_file = blocks.MODEL_SAVE_PATH + '/scores/' + name + '.txt'
-        # model = simple_model(x_train, y_train, x_test, y_test, vars_out, score_file)
-        # save_file = blocks.MODEL_SAVE_PATH + name + '.p'
-        # save_model(model, save_file)
-
-        #lasagne model
-        # name = 'lasagne_model_1_{0}'.format(block_name)
-        # print('lasagne model {0}'.format(name))
-        # score_file = blocks.MODEL_SAVE_PATH + '/scores/' + name + '.txt'
-        # model = lasagne_model(np.asarray(x_train), np.asarray(y_train), np.asarray(x_test), np.asarray(y_test), vars_out, score_file)
-        # save_file = blocks.MODEL_SAVE_PATH + name + '.p'
-        # save_model(model, save_file)
 
         #keras model
         name = 'keras_model_test_{0}'.format(block_name)
         print('keras model {0}'.format(name))
         score_file = blocks.MODEL_SAVE_PATH + '/scores/' + name + '.txt'
-        model = keras_model(np.asarray(x_train), np.asarray(y_train), np.asarray(x_test), np.asarray(y_test), vars_out, score_file)
         save_file = blocks.MODEL_SAVE_PATH + name + '.p'
         save_model(model, save_file)
 
