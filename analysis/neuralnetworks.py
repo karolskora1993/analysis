177001264f20dc713621d4413ca4d3c388ccdee5
karolskora1993 <karol-skora@poczta.pl>
2017-09-11 13:56:14 +0200

    add data shift

--- analysis/neuralnetworks.py
+++ analysis/neuralnetworks.py
@@ -11,9 +11,9 @@ import os
 
 sys.setrecursionlimit(10000)
 
-COL_NAMES = ['in', 'control', 'out']
-LAST_TRAIN_IDX = 205.038
-LAST_VALIDATE_IDX = 257.133
+COL_NAMES = ['in', 'control', 'out', 'delay']
+LAST_TRAIN_IDX = 205038
+LAST_VALIDATE_IDX = 257133
 
 
 HOME_PATH = str(os.path.expanduser('~')+'/')
@@ -21,7 +21,7 @@ HOME_PATH = str(os.path.expanduser('~')+'/')
 LOAD_PATH = HOME_PATH + '/Dokumenty/analysis/data/bloki_v4/'
 MODEL_SAVE_PATH = HOME_PATH + '/Dokumenty/analysis/data/models/'
 
-BLOCK_VARS_PATH_XLSX = HOME_PATH + '/Dokumenty/analysis/data/bloki_poprawione.xlsx'
+BLOCK_VARS_PATH_XLSX = HOME_PATH + '/Dokumenty/analysis/data/bloki_poprawione_v3.xlsx'
 
 BLOCK_NAMES = [
     'blok I',
@@ -44,21 +44,25 @@ class SimpleTester(ModelTester):
 
 
 class Model(ABC):
-    def __init__(self, input_data, output_data, model_tester):
+    def __init__(self, input_data, output_data, last_train_index, last_validate_index, model_tester):
         self._input_data = input_data
         self._output_data = output_data
         self._model_tester = model_tester
         self._model = None
+        self._last_train_idx = last_train_index
+        self._last_validate_idx = last_validate_index
         self.validate_data()
 
     def validate_data(self):
-        if np.any(np.isnan(self.input_data)) or np.any(np.isnan(self._output_data)):
+        if np.any(np.isnan(self._input_data)) or np.any(np.isnan(self._output_data)):
             raise ValueError('Data contains NaN values')
-        if not np.all(np.isfinite(self.input_data)) or not np.all(np.isfinite(self._output_data)):
+        if not np.all(np.isfinite(self._input_data)) or not np.all(np.isfinite(self._output_data)):
             raise ValueError('Data contains infinite values')
 
-    def test_model(self, x_test, y_test):
-        return self._model_tester.test(self._model, x_test, y_test)
+    def test_model(self):
+        r2_test = self._model_tester.test_model(self._model, self._input_data[self._last_validate_idx: ], self._output_data[self._last_validate_idx:])
+        r2_train = self._model_tester.test_model(self._model, self._input_data[:self._last_train_idx], self._output_data[:self._last_train_idx])
+        return (r2_test, r2_train)
 
     def get_model(self):
         return self._model
@@ -68,7 +72,7 @@ class Model(ABC):
         pass
 
     @abstractmethod
-    def train_model(self, x_train, y_train, validation_data=None, epochs=500):
+    def train_model(self, epochs=500):
         pass
 
     @abstractmethod
@@ -76,9 +80,9 @@ class Model(ABC):
         pass
 
 
-
 class KerasMLPModel(Model):
     def create_model(self, input_size, output_size, network_shape=(10,), optimizer='adam', loss='mean_squared_error'):
+        print('input_size: {0} output_size: {1} network_shape: {2}'.format(input_size, output_size, network_shape))
         self._model = Sequential()
 
         network_shape = network_shape if network_shape is not None and isinstance(network_shape, tuple) else (10,)
@@ -87,14 +91,20 @@ class KerasMLPModel(Model):
         self._model.add(Dropout(0.5))
 
         for layer in network_shape:
-            self._model.add(Dense(layer, activation='relu'))
-            self._model.add(Dropout(0.5))
+            if layer > 0:
+                self._model.add(Dense(layer, activation='relu'))
+                self._model.add(Dropout(0.5))
 
         self._model.add(Dense(output_size, activation='linear'))
         self._model.compile(optimizer=optimizer, loss=loss)
         print('Model created')
 
-    def train_model(self, x_train, y_train, validation_data, epochs=500):
+    def train_model(self, epochs=500):
+        x_train = self._input_data[:self._last_train_idx]
+        y_train = self._output_data[:self._last_train_idx]
+        validation_data = (self._input_data[self._last_train_idx: self._last_validate_idx],
+                            self._output_data[self._last_train_idx: self._last_validate_idx])
+
         scaler = StandardScaler().fit(x_train)
         x_train = scaler.transform(x_train)
         self._model.fit(x_train, y_train, epochs=epochs, batch_size=32, verbose=2, validation_data=validation_data)
@@ -120,40 +130,44 @@ def save_model(model, save_path):
     pickle.dump(model, open(save_path, 'wb'))
     print('Model saved')
 
+
 def get_network_shape():
     network_shape = (10, 0)
     if len(sys.argv) > 1:
         cmd_line_args = []
-        for i, arg in sys.argv.enumerate():
+        for i, arg in enumerate(sys.argv):
             if i > 0:
                 cmd_line_args.append(int(arg))
         network_shape = tuple(cmd_line_args)
-    print('Network shape: {0}'.format(network_shape))
     return network_shape
 
-
+def shift_Data(input_data, output_data, delay):
+    input_data = input_data[: -delay]
+    output_data = output_data[delay:]
+    return (input_data, output_data)
 
 def model_block(data, var_names):
     vars_in = var_names['in'].append(var_names['control']).dropna().tolist()
     vars_out = var_names['out'].dropna().tolist()
+    delays = var_names['delay']
     block_models = []
     network_shape = get_network_shape()
-    input_data = data[vars_in].as_matrix().transpose()
-    for var_out in vars_out:
+    input_data = data[vars_in].as_matrix()
+    for i, var_out in enumerate(vars_out):
         print('var_out:\t{0}'.format(var_out))
-        output_data = data[var_out].as_matrix().transpose()
-        model = KerasMLPModel(input_data, output_data, SimpleTester())
-        model.create_model(input_data.shape[1], output_data, network_shape)
-        model.train_model(input_data[:LAST_TRAIN_IDX], output_data[LAST_TRAIN_IDX],
-                          (input_data[LAST_TRAIN_IDX: LAST_VALIDATE_IDX], output_data[LAST_TRAIN_IDX: LAST_VALIDATE_IDX]))
-        r2 = model.test_model(input_data[LAST_VALIDATE_IDX:], output_data[LAST_VALIDATE_IDX:])
+        output_data = data[var_out].as_matrix()
+        delay = int(delays[i]) if delays[i] >= 1 else 0
+        input_data, output_data = shift_Data(input_data, output_data, delay)
+        model = KerasMLPModel(input_data, output_data, LAST_TRAIN_IDX, LAST_VALIDATE_IDX, SimpleTester())
+        model.create_model(input_data.shape[1], 1, network_shape)
+        model.train_model()
+        r2 = model.test_model()
         block_models.append({'output': var_out, 'model': model.get_model()})
-        print('r^2:\t {0}'.format(r2))
+        print('r^2_test:\t {0} \n r^2_train:\t{1}'.format(r2[0], r2[1]))
         save_path = MODEL_SAVE_PATH + var_out + '.p'
         save_model(model, save_path)
 
     return block_models
-
 
 
 def main():
